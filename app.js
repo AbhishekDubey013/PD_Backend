5f122deeb384afa2d810f012c2461eefb1b32ba0
Abhishek <abhishekdubey013@gmail.com>
2023-07-19 21:02:08 +0530

    m

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ client.on('message', (message) => {
 });
 
 app.get('/', (req, res) => {
+  res.send("IT WORKED");
   if (qrCodeImage) {
     res.send(`<img src="${qrCodeImage}" alt="QR Code">`);
   } else {
