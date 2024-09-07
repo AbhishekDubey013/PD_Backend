7774566bf6ac39f20ffaf4dde365e0dd62e0b8e9
Abhishek <abhishekdubey013@gmail.com>
2024-09-07 13:48:52 +0530

    pf

---
+++ prev.js
@@ -0,0 +1,130 @@
+// const express = require('express');
+// //const { Client } = require('whatsapp-web.js');
+// const qrcode = require('qrcode');
+// const { ethers, JsonRpcProvider } = require('ethers');
+// const { Configuration, OpenAIApi } = require('openai');
+// const axios = require('axios');
+// const MongoClient = require('mongodb').MongoClient;
+// const bodyParser = require('body-parser');
+// require('dotenv').config();
+// const app = express();
+// // Ethereum setup
+// const provider = new JsonRpcProvider(process.env.GOERLI_URL);
+// const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+// const contractAddress = "0xB59f5DD1bC4BF95073C2CFF995b6025BC67b1C1F";
+// const contractABI = require('./smContract/chai.json');
+// //const questionnaire = require('./smContract/Subjective.json')
+// const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+// const questionsData = require('./whatsappbot/Objective.json');
+// const pers = require('./whatsappbot/Subjective.json');
+// // const client = new Client();
+// let qrCodeImage = null;
+// const checkFlagInterval = 15000;
+// app.use(bodyParser.json());
+// const cors = require("cors");
+// app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin","https://psychdoc.in");
+//   //"https://psychdoc.in"
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+// app.use(express.json())
+
+// const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
+// const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+
+
+
+// // API endpoint to initiate a blockchain transaction
+// app.post('/buy-chai', async (req, res) => {
+//   const { name, data2, phoneNumber, moduleTestName, moduleName } = req.body;
+//   console.log(moduleName)
+//   try {
+//     // Perform blockchain transaction
+//     const tx = await contract.buyChai(name, data2, {
+//       value: ethers.parseEther("0.001")
+//     });
+//     await tx.wait();
+
+
+  
+//     // Write result to MongoDB
+//     await mongoClient.connect();
+//     const db = mongoClient.db("yourDatabaseName");
+//     const results = db.collection("results");
+//     await results.insertOne({ phoneNumber, analysisResult, timestamp: new Date() });
+
+//     res.send({ success: true, message: 'Transaction and analysis successful', analysis: analysisResult, transactionId: tx.hash });
+//   } catch (error) {
+//     console.error('Error during processing:', error);
+//     res.status(500).send({ success: false, message: 'Processing failed', error: error.message });
+//   } finally {
+//     await mongoClient.close();
+//   }
+// });
+
+// async function checkFlagAndSendMessage() {
+//   try {
+//     console.log("Fetching data from API...");
+//     const { data } = await axios.get('https://mongodb-ttio.onrender.com/api/auth/pdh', { timeout: 5000 });
+//     console.log("Data received:", data);
+
+//     for (const entry of data) {
+//       const questions = questionsData[entry.moduleName];
+//       const question = pers[entry.moduleName];
+//       console.log("Processing entry:", entry);
+//       const response = await axios.get(`https://mongodb-ttio.onrender.com/api/auth/adh?PK=${entry.PK}`, { timeout: 5000 });
+//       console.log("hello", entry)
+//       const data1 = response.data;
+//       console.log("Data received:", data1);
+  
+
+//       let introduction = `Assessment: ${entry.moduleName}. Key observations gathered from responses are as follows:`;
+//       let promptForDiagnosis = "Based on the above observations, please provide a concise diagnostic impression. Include a comment on the probable feelings or psychological states of the individual along with an estimated probability(in numeric percentage) of this diagnosis. Ensure this is formatted in an easy-to-understand manner for someone without a medical background.";
+//       let combinedString = `${introduction}\n\n${entry.dataArray.map((response, index) => `${question[index]}: ${response}`).join('\n')}\n\nFurther information based on additional data:\n${data1[0].dataArray.map((response, index) => `${questions[index]}: ${response}`).join('\n')}\n\n${promptForDiagnosis}`;
+      
+
+//       console.log("Combined string:", combinedString);
+
+//       const completion = await openai.createCompletion({
+//         model: 'gpt-3.5-turbo-instruct',
+//         prompt: combinedString,
+//         max_tokens: 200,
+//       });
+
+//       console.log("OpenAI response:", completion.data.choices[0].text);
+
+//       const analysisResult = completion.data.choices[0].text;
+//       const whatsappNumber = entry.mobileNumber;
+//       const formattedPhoneNumber = `91${whatsappNumber}@c.us`;
+
+//       const updateResponse = await axios.put('https://mongodb-ttio.onrender.com/api/auth/pp', {
+//         _id: entry._id,
+//         newFlag: 'N'
+//       }, { timeout: 5000 });
+//       console.log("Database update response:", updateResponse.data);
+
+//       // Write result to MongoDB
+//       await mongoClient.connect();
+//       const db = mongoClient.db("yourDatabaseName");
+//       const results = db.collection("results");
+//       await results.insertOne({ formattedPhoneNumber, analysisResult, timestamp: new Date() });
+//     }
+//   } catch (error) {
+//     console.error('Error in checkFlagAndSendMessage:', error);
+//   }
+// }
+
+
+// // setInterval(syncWithDatabase, syncInterval);
+// setInterval(checkFlagAndSendMessage, checkFlagInterval);
+
+// // Start the server
+// const server = app.listen(process.env.PORT || 3002, () => {
+//   console.log(`Server is running on port ${server.address().port}`);
+// });
